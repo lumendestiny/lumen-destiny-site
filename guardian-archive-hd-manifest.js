e001e23accbb811936831b3d892fe374660b461f
lumendestiny <llumendestiny@gmail.com>
2026-08-14 22:50:25 +0700

    Add 20-slot Guardian archive HD manifest

---
+++ guardian-archive-hd-manifest.js
@@ -0,0 +1,26 @@
+window.LUMEN_GUARDIAN_ARCHIVE_HD = Object.freeze({
+  version: '2026-08-14-hd20-stage1',
+  basePath: '/assets/guardian/archive-hd/',
+  items: {
+    'fortune-cat': null,
+    'koi': null,
+    'sun-bird': null,
+    'new-deer': null,
+    'gold-hamster': null,
+    'moon-rabbit': null,
+    'dolphin': null,
+    'fire-fox': null,
+    'leaf-turtle': null,
+    'star-owl': null,
+    'nine-fox': null,
+    'sea-dragon': null,
+    'unicorn': null,
+    'forest-turtle': null,
+    'wing-owl': null,
+    'sky-dragon': null,
+    'fire-phoenix': null,
+    'moon-tiger': null,
+    'qilin': null,
+    'black-turtle': null
+  }
+});
