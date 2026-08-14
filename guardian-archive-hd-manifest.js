8a3e3752a232b3681b747621a3c01ba2a140868f
lumendestiny <llumendestiny@gmail.com>
2026-08-15 00:02:10 +0700

    Enable approved Basic Guardian archive HD artwork

--- guardian-archive-hd-manifest.js
+++ guardian-archive-hd-manifest.js
@@ -1,12 +1,12 @@
 window.LUMEN_GUARDIAN_ARCHIVE_HD = Object.freeze({
-  version: '2026-08-14-hd20-stage1',
+  version: '2026-08-14-hd20-basic-live-1',
   basePath: '/assets/guardian/archive-hd/',
   items: {
-    'fortune-cat': null,
-    'koi': null,
-    'sun-bird': null,
-    'new-deer': null,
-    'gold-hamster': null,
+    'fortune-cat': 'guardian-basic-fortune-cat-hd.webp',
+    'koi': 'guardian-basic-koi-hd.webp',
+    'sun-bird': 'guardian-basic-sun-bird-hd.webp',
+    'new-deer': 'guardian-basic-new-deer-hd.webp',
+    'gold-hamster': 'guardian-basic-gold-hamster-hd.webp',
     'moon-rabbit': null,
     'dolphin': null,
     'fire-fox': null,
